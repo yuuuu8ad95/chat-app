ba0b07fe4ee8c54ac38dd99255f76ee41e6717d9
yuuuu8ad95 <yu.i08elle@icloud.com>
2020-08-10 18:18:40 +0900

    新規作成画面の表示

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,5 @@ Rails.application.routes.draw do
   get 'messages/index'
   root "messages#index"
   resources :users, only: [:edit, :update]
+  resources :rooms, only: [:new, :create]
 end
--- db/migrate/20200810085021_create_rooms.rb
+++ db/migrate/20200810085021_create_rooms.rb
@@ -2,7 +2,6 @@ class CreateRooms < ActiveRecord::Migration[6.0]
   def change
     create_table :rooms do |t|
       t.string :name, null: false
-
       t.timestamps
     end
   end
