3bc69cd0c4b1f1a40c7aa0aa47c9362e59851092
yuuuu8ad95 <yu.i08elle@icloud.com>
2020-08-11 18:15:04 +0900

    メッセージ送信機能の実装

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -2,6 +2,7 @@ class MessagesController < ApplicationController
   def index
     @message = Message.new
     @room = Room.find(params[:room_id])
+    @messages = @room.messages.includes(:user)
   end
 
   def create
@@ -17,7 +18,7 @@ class MessagesController < ApplicationController
   private
 
   def message_params
-    params.require(:message).permit(:contenc).merge(user_id: current_user.id)
+    params.require(:message).permit(:content).merge(user_id: current_user.id)
   end
 end
 
